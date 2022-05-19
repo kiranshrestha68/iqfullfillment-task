8fa98e0038a1052b2c29fe70b681f17e8488a3a1
kiranshrestha68 <kiranstha68@gmail.com>
2022-05-19 10:42:11 +0545

    line chart added

---
+++ src/components/Linechart.vue
@@ -0,0 +1,22 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  props: {
+    label: {
+      type: String,
+    },
+    charData: {
+      type: Array,
+    },
+    options: {
+      type: Object,
+    },
+  },
+
+  mounted(){
+      
+  }
+};
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -21,20 +21,42 @@
           <div class="home__total">
             <div class="home__totalIndividual">
               <img :src="require('../assets/staff1.svg')" alt="" />
-              <div class="home__totalIndividuals"><span>Total Patients</span> <span>24,250</span></div>
+              <div class="home__totalIndividuals">
+                <span>Total Patients</span> <span>24,250</span>
+              </div>
             </div>
 
             <div class="home__totalIndividual">
               <img :src="require('../assets/staff1.svg')" alt="" />
-              <div class="home__totalIndividuals"><span>Total Patients</span> <span>24,250</span></div>
+              <div class="home__totalIndividuals">
+                <span>Total Patients</span> <span>24,250</span>
+              </div>
             </div>
 
-             <div class="home__totalIndividual">
+            <div class="home__totalIndividual">
               <img :src="require('../assets/staff1.svg')" alt="" />
-              <div class="home__totalIndividuals"><span>Total Patients</span> <span>24,250</span></div>
+              <div class="home__totalIndividuals">
+                <span>Total Patients</span> <span>24,250</span>
+              </div>
             </div>
           </div>
-          <div class="home__lineChart"></div>
+          <div class="home__lineChart">
+            <div class="linechart__headers">
+              <h3>Patients Covid Statistics</h3>
+              <div class="linechart__buttons">
+                <button class="linechart__monthly">Monthly</button>
+                <button class="linechart__weekly">Weekly</button>
+              </div>
+            </div>
+            <div class="linechart__body">
+              <!-- <Linechart/> -->
+              <GChart
+                type="LineChart"
+                :options="options"
+                :data="collectionData"
+              />
+            </div>
+          </div>
         </div>
         <div class="home__col2"></div>
       </div>
@@ -44,11 +66,39 @@
 
 <script>
 import Topbar from "../components/Topbar.vue";
+// import Linechart from "../components/Linechart.vue";
+import { GChart } from "vue-google-charts/legacy";
 
 export default {
   name: "Home",
-  components: { Topbar },
+  components: {
+    Topbar,
+    GChart,
+  },
   props: {},
+
+  data() {
+    return {
+      collectionData: [
+        ["Day", "Guardians of the Galaxy", "The Avengers", "Transformers"],
+        [1, 40.45, 90.5, 42.8],
+        [2, 22.5, 68.4, 33.4],
+        [3, 36.5, 47, 55.5],
+        [4, 12.7, 23.8, 14.5],
+        [5, 10.9, 44.5, 10.3],
+        [6, 7.8, 14.5, 6.7],
+        [7, 8.6, 11.2, 19.6],
+      ],
+      options: {
+        chart: {
+          title: "First 7 days movies collection",
+          subtitle: "In millions of dollars (USD)",
+        },
+        width: 1000,
+        height: 400,
+      },
+    };
+  },
 };
 </script>
 
@@ -126,13 +176,13 @@ export default {
 }
 
 .home__col1 {
-  border: 1px solid black;
+  /* border: 1px solid rgb(99, 22, 22); */
   flex: 8.5;
   margin-right: 25px;
 }
 
 .home__total {
-  border: 1px solid black;
+  /* border: 1px solid black; */
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -149,25 +199,72 @@ export default {
   border-radius: 15px;
 }
 
-.home__totalIndividual>img{
+.home__totalIndividual > img {
   height: 50px;
   padding-top: 15px;
 }
 
-.home__totalIndividuals{
+.home__totalIndividuals {
   display: flex;
   flex-direction: column;
   margin-left: 15px;
 }
 
 .home__lineChart {
-  border: 1px solid black;
+  /* border: 1px solid black; */
   min-height: 40vh;
-  margin-top: 25px;
+  /* margin-top: 25px; */
+  background: white;
+  padding: 1.5%;
+  border-radius: 15px;
+  margin: 20px 8px;
 }
 
 .home__col2 {
   border: 1px solid rgb(185, 31, 31);
   flex: 3.5;
+}
+
+.linechart__headers {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.linechart__headers > h3 {
+  font-family: "Montserrat", sans-serif;
+  font-size: 15px;
+  font-weight: bolder;
+  letter-spacing: 0.8px;
+}
+
+.linechart__buttons {
+  /* border: 1px solid yellow; */
+  padding: 8px 5px;
+  border-radius: 12px;
+  font-family: "Montserrat", sans-serif;
+  background-color: #f4f7fc;
+}
+
+.linechart__monthly {
+  border: none;
+  background-color: white;
+  color: blue;
+  font-weight: bolder;
+  font-size: 14px;
+  margin: 0 6px;
+  padding: 8px 15px;
+  border-radius: 8px;
+}
+
+.linechart__weekly {
+  border: none;
+  background-color: transparent;
+  color: rgb(87, 87, 87);
+
+  font-size: 14px;
+  margin: 0 6px;
+  padding: 8px 15px;
+  border-radius: 8px;
 }
 </style>
