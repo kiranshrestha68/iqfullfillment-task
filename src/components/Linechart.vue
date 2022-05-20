18c2b0a5a6a4ddb76e23fdbc43800fd19b5d510d
kiranshrestha68 <kiranstha68@gmail.com>
2022-05-20 05:49:16 +0545

    line chart completed

--- src/components/Linechart.vue
+++ src/components/Linechart.vue
@@ -1,22 +1,75 @@
-<script>
-import { Line } from "vue-chartjs";
+<template>
+  <div>
+    <canvas id="planet-chart"></canvas>
+  </div>
+</template>
 
-export default {
-  extends: Line,
-  props: {
-    label: {
-      type: String,
-    },
-    charData: {
-      type: Array,
-    },
-    options: {
-      type: Object,
+<script>
+import Chart from "chart.js/dist/chart";
+
+export const planetChartData = {
+  type: "line",
+  data: {
+    labels: [
+      "Mercury",
+      "Venus",
+      "Earth",
+      "Mars",
+      "Jupiter",
+      "Saturn",
+      "Uranus",
+      "Neptune",
+    ],
+    datasets: [
+      {
+        label: "Number of Moons",
+        data: [0, 0, 1, 2, 79, 82, 27, 14],
+        backgroundColor: "#1fadd8",
+        borderColor: "#1fadd8",
+        borderWidth: 2,
+      },
+      {
+        label: "Planetary Mass",
+        data: [0.166, 2.081, 3.003, 0.323, 954.792, 285.886, 43.662, 51.514],
+        backgroundColor: "blue",
+        borderColor: "blue",
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    lineTension: 1,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            padding: 25,
+          },
+        },
+      ],
     },
   },
+};
 
-  mounted(){
-      
-  }
+export default {
+  name: "Linechart",
+  data() {
+    return {
+      planetChartData: planetChartData,
+    };
+  },
+
+  mounted() {
+    const ctx = document.getElementById("planet-chart");
+    new Chart(ctx, this.planetChartData);
+  },
 };
 </script>
+
+<style scoped>
+#planet-chart {
+  color: black;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -48,14 +48,7 @@
                 <button class="linechart__weekly">Weekly</button>
               </div>
             </div>
-            <div class="linechart__body">
-              <!-- <Linechart/> -->
-              <GChart
-                type="LineChart"
-                :options="options"
-                :data="collectionData"
-              />
-            </div>
+            <div class="linechart__body"><Linechart /></div>
           </div>
         </div>
         <div class="home__col2"></div>
@@ -66,38 +59,27 @@
 
 <script>
 import Topbar from "../components/Topbar.vue";
-// import Linechart from "../components/Linechart.vue";
-import { GChart } from "vue-google-charts/legacy";
+import Linechart from "../components/Linechart.vue";
+// import { GChart } from "vue-google-charts/legacy";
+import axios from "axios";
 
 export default {
   name: "Home",
   components: {
     Topbar,
-    GChart,
+    Linechart,
   },
   props: {},
 
   data() {
-    return {
-      collectionData: [
-        ["Day", "Guardians of the Galaxy", "The Avengers", "Transformers"],
-        [1, 40.45, 90.5, 42.8],
-        [2, 22.5, 68.4, 33.4],
-        [3, 36.5, 47, 55.5],
-        [4, 12.7, 23.8, 14.5],
-        [5, 10.9, 44.5, 10.3],
-        [6, 7.8, 14.5, 6.7],
-        [7, 8.6, 11.2, 19.6],
-      ],
-      options: {
-        chart: {
-          title: "First 7 days movies collection",
-          subtitle: "In millions of dollars (USD)",
-        },
-        width: 1000,
-        height: 400,
-      },
-    };
+    return {};
+  },
+
+  async created() {
+    const res = await axios.get(
+      "https://apitest.iqfulfillment.com/v1/test/dashboard"
+    );
+    console.log(res, "hellot his is ers");
   },
 };
 </script>
